46b0919f02d3478f4132f081123d1301ef642b03
Rawand Sami abu-shammala <98568844+Rawand-abu-shammala@users.noreply.github.com>
2025-11-25 14:39:46 +0200

    Delete screens directory

--- screens/About.screen.tsx
+++
@@ -1,8 +0,0 @@
-export const About = () => {
-  return (
-    <div className="about-screen">
-        <h2>About Student App</h2>
-        <p>This is an app that we use to explain topics of react in GSG training.</p>
-    </div>
-  )
-}
--- screens/NotFound.screen.tsx
+++
@@ -1,9 +0,0 @@
-export const NotFound = () => {
-  return (
-    <>
-    <h2>page Not Found (404)</h2>
-    <p>we can't find the page you are looking for</p>
-    
-    </>
-  )
-}
